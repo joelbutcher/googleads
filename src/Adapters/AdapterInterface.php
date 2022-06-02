f083e2c5c191f7ee89438180504d4aac939e491c
Joel Butcher <joelbutcher@users.noreply.github.com>
2022-06-02 07:10:06 +0100

    Update AdapterInterface.php

--- src/Adapters/AdapterInterface.php
+++ src/Adapters/AdapterInterface.php
@@ -2,7 +2,7 @@
 
 namespace JoelButcher\GoogleAds\Adapters;
 
-use Google\Ads\GoogleAds\Lib\V7\GoogleAdsClient as V7Client;
+use Google\Ads\GoogleAds\Lib\V10\GoogleAdsClient as V10Client;
 use Google\Ads\GoogleAds\Lib\V8\GoogleAdsClient as V8Client;
 use Google\Ads\GoogleAds\Lib\V9\GoogleAdsClient as V9Client;
 
@@ -13,7 +13,7 @@ interface AdapterInterface
      *
      * @param  string  $refreshToken
      * @param  int|null  $loginCustomerId
-     * @return V7Client|V8Client|V9Client
+     * @return V8Client|V9Client|V10Client
      */
     public function getClient(string $refreshToken, ?int $loginCustomerId = null);
 }
